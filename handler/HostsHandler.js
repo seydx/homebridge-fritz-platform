b960907ed5822d33dc6bd555c75c50b1b443659a
Seyd <34152761+SeydX@users.noreply.github.com>
2019-05-11 03:08:56 +0200

    Update HostsHandler.js

--- handler/HostsHandler.js
+++ handler/HostsHandler.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const api = require('../lib/TR064.js');
+
 const axios = require('axios');
 const parseString = require('xml2js').parseString;
 
 class HostsHandler {
-  constructor (platform, config, device) {
+  constructor (platform, config, device, devices, mainConfig) {
 
     this.log = platform.log;
     this.logger = platform.logger;
@@ -13,11 +15,169 @@ class HostsHandler {
     this.platform = platform;
 
     this.config = config;
-    this.device = device;
+    this.mainConfig = mainConfig;
+    this.device = device;    
+    this.devices = devices;
     
-    this.generateHostsListURL();
+    this.activeDevices = [];
+    
+    if(config.mesh){
+      
+      this.generateHostsListURL();
+
+    } else {
+    
+      this.initDevices();  
+    
+    }
 
   }
+  
+  //NO MESH
+  
+  async initDevices(){
+  
+    try {
+
+      let devices = [];
+      let hostLists = [];
+      let UserList = [];
+  
+      for(const device of Object.keys(this.devices)){
+    
+        if(this.devices[device].host && this.devices[device].port && this.devices[device].username && this.devices[device].password && this.devices[device].type)
+          devices.push({
+            name: device,
+            host: this.devices[device].host,
+            port: this.devices[device].port,
+            username: this.devices[device].username,
+            password: this.devices[device].password,
+            timeout: this.mainConfig.timeout * 1000,
+            type: this.devices[device].type
+          });
+    
+      }
+  
+      for(const config of devices){
+
+        if(!this.activeDevices[config.name]){    
+      
+          let dev = await this.generateApi(config);
+          this.activeDevices[config.name] = dev;
+      
+        }
+
+      }  
+
+      for(const activeDevice of Object.keys(this.activeDevices)){
+        
+        let list = await this.getHosts(this.activeDevices[activeDevice]);
+        
+        if(list)
+          hostLists.push(list);
+     
+      }
+      
+      
+      for(const list in hostLists)
+        for(const user in hostLists[list])
+          UserList.push(hostLists[list][user]);
+          
+      this.hosts = UserList;
+
+    } catch(err){
+
+      this.logger.error('Host List (no mesh): An error occured while fetching devices!');
+      this.debug(JSON.stringify(err, null, 4));
+
+    } finally {
+
+      setTimeout(this.initDevices.bind(this), this.mainConfig.polling * 1000);
+
+    }
+  
+  }
+  
+  async generateApi(config){
+  
+    try{
+    
+      let TR064 = new api.TR064(config, this.logger);  
+      TR064 = await TR064.initDevice();
+        
+      let device = await TR064.startEncryptedCommunication();
+    
+      return device;
+    
+    } catch(err){
+    
+      throw err;
+    
+    }
+
+  }
+  
+  async getHosts(device){
+  
+    try {
+    
+      let hosts = device.services['urn:dslforum-org:service:Hosts:1'];
+      hosts = await hosts.actions['X_AVM-DE_GetHostListPath']();
+      
+      let list = await this.generateHostListNM(hosts['NewX_AVM-DE_HostListPath'], device);
+      
+      return list;
+    
+    } catch(err){
+    
+      throw err;
+    
+    }
+  
+  }
+  
+  async generateHostListNM(endpoint, device){
+  
+    try {
+    
+      let ping = await this.tcp('Presence', device.config.host, device.config.port);
+      
+      if(ping){
+    
+        let url = 'http://' + device.config.host + ':' + device.config.port + endpoint;
+        let hostArray = [];
+      
+        let hostList = await axios(url);  
+        let hostListXML = await this.xmlParser(hostList.data);
+        
+        hostArray = hostArray.concat(hostListXML.List.Item);
+        
+        return hostArray;
+        
+      } else {
+      
+        this.debug('Host List (no mesh): Network currently not available!');
+        return false;
+        
+      }
+    
+    } catch(error){
+    
+      if(error.response)
+        error = {
+          status: error.response.status,
+          message: error.response.statusText,
+          config: error.config,
+          data: error.response.data
+        };
+      
+      throw error;
+    
+    }
+  
+  }
+  
+  //MESH
   
   async generateHostsListURL(){
   
