4c72dddbf11a48e80be6678feedb75c8a920946f
Seyd <34152761+SeydX@users.noreply.github.com>
2019-05-20 20:43:27 +0200

    Update ConfigHandler.js

--- handler/ConfigHandler.js
+++ handler/ConfigHandler.js
@@ -29,7 +29,7 @@ class DeviceHandler {
       this.config.timeout = this.config.timeout||10;
       this.config.clearCache = this.config.clearCache||false;
       this.config.debug = this.config.debug||false;
-      this.config.disableAutoConfig = this.config.disableAutoConfig||false;
+      this.config.disableAutoSearch = this.config.disableAutoSearch||false;
       
       this.config.devices = this.config.devices||{};
       this.config.smarthome = this.config.smarthome||{};
