d090cd4e3613ee0d66cd34b8613d49a588bd3f87
Seyd <34152761+SeydX@users.noreply.github.com>
2020-11-06 23:27:05 +0100

    Reset lastSeen for presence motion

--- src/accessories/presence/motion.js
+++ src/accessories/presence/motion.js
@@ -41,6 +41,8 @@ class PresenceMotionAccessory {
     
     this.historyService = new this.FakeGatoHistoryService('motion', this.accessory, {storage:'fs', path: this.api.user.storagePath() + '/fritzbox/'}); 
     
+    this.accessory.context.lastSeen = false;
+    
     if(this.accessory.displayName === 'Anyone' || (this.accessory.context.polling.timer && (!this.accessory.context.polling.exclude.includes(this.accessory.context.config.type) && !this.accessory.context.polling.exclude.includes(this.accessory.context.config.subtype) && !this.accessory.context.polling.exclude.includes(this.accessory.displayName)))){
    
       service.getCharacteristic(this.api.hap.Characteristic.MotionDetected)
