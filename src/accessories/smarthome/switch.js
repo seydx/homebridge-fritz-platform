61ed19501a1fdb96f171815069de088bf2439352
Seyd <34152761+SeydX@users.noreply.github.com>
2020-11-06 11:30:12 +0100

    Fix „smarthome switch“ polling

--- src/accessories/smarthome/switch.js
+++ src/accessories/smarthome/switch.js
@@ -28,7 +28,7 @@ class extrasService {
       service = this.accessory.addService(this.api.hap.Service.Switch, this.accessory.displayName, this.accessory.context.config.subtype);
     }
     
-    if(this.accessory.context.polling.timer && (!this.accessory.context.polling.exclude.includes(this.accessory.context.config.type) || !this.accessory.context.polling.exclude.includes(this.accessory.context.config.subtype) || !this.accessory.context.polling.exclude.includes(this.accessory.displayName))){
+    if(this.accessory.context.polling.timer && (!this.accessory.context.polling.exclude.includes(this.accessory.context.config.type) && !this.accessory.context.polling.exclude.includes(this.accessory.context.config.subtype) && !this.accessory.context.polling.exclude.includes(this.accessory.displayName))){
  
       service.getCharacteristic(this.api.hap.Characteristic.On)
         .on('set', this.handler.set.bind(this, this.accessory, this.api.hap.Service.Switch, this.api.hap.Characteristic.On, 'smarthome-switch', this.accessory.context.config.options));
