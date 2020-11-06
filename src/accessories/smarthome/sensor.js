4a0a744f9183c734cd27f35683590a04c6a3821c
Seyd <34152761+SeydX@users.noreply.github.com>
2020-11-06 11:29:17 +0100

    Fix „smarthome contact“ polling

--- src/accessories/smarthome/sensor.js
+++ src/accessories/smarthome/sensor.js
@@ -62,7 +62,7 @@ class extrasService {
     
     this.historyService = new this.FakeGatoHistoryService('door', this.accessory, {storage:'fs', path: this.api.user.storagePath() + '/fritzbox/'}); 
     
-    if(this.accessory.context.polling.timer && (!this.accessory.context.polling.exclude.includes(this.accessory.context.config.type) || !this.accessory.context.polling.exclude.includes(this.accessory.context.config.subtype) || !this.accessory.context.polling.exclude.includes(this.accessory.displayName))){
+    if(this.accessory.context.polling.timer && (!this.accessory.context.polling.exclude.includes(this.accessory.context.config.type) && !this.accessory.context.polling.exclude.includes(this.accessory.context.config.subtype) && !this.accessory.context.polling.exclude.includes(this.accessory.displayName))){
  
       service.getCharacteristic(this.api.hap.Characteristic.ContactSensorState)
         .on('change', this.handler.change.bind(this, this.accessory, 'smarthome-contact', this.accessory.displayName, this.historyService));
