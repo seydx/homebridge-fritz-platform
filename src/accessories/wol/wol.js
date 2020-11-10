95eed544065878f8327cb054459aec13f46b9db8
Seyd <34152761+SeydX@users.noreply.github.com>
2020-11-10 10:16:57 +0100

    Fix wol switch

--- src/accessories/wol/wol.js
+++ src/accessories/wol/wol.js
@@ -19,19 +19,19 @@ class WolSwitchAccessory {
   // Services
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
-  getService (accessory) {
+  getService () {
     
-    let service = accessory.getService(this.api.hap.Service.Switch);
+    let service = this.accessory.getService(this.api.hap.Service.Switch);
     
     if(!service){
-      Logger.info('Adding Switch service', accessory.displayName);
-      service = accessory.addService(this.api.hap.Service.Switch, this.accessory.displayName, this.accessory.context.config.subtype);
+      Logger.info('Adding Switch service', this.accessory.displayName);
+      service = this.accessory.addService(this.api.hap.Service.Switch, this.accessory.displayName, this.accessory.context.config.subtype);
     }
   
     service
       .getCharacteristic(this.api.hap.Characteristic.On)
-      .on('get', this.handler.get.bind(this, accessory, this.api.hap.Service.Switch, this.api.hap.Characteristic.On, 'wol', false))
-      .on('set', this.handler.set.bind(this, accessory, this.api.hap.Service.Switch, this.api.hap.Characteristic.On, 'wol', false));
+      .on('get', this.handler.get.bind(this, this.accessory, this.api.hap.Service.Switch, this.api.hap.Characteristic.On, 'wol', false))
+      .on('set', this.handler.set.bind(this, this.accessory, this.api.hap.Service.Switch, this.api.hap.Characteristic.On, 'wol', false));
     
   }
 
