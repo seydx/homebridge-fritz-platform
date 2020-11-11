b24ca1ce903c6af005c97c20b60283468a33d2a0
Seyd <34152761+SeydX@users.noreply.github.com>
2020-11-11 18:39:43 +0100

    ReadOnly for smarthome switch

--- src/accessories/smarthome/switch.js
+++ src/accessories/smarthome/switch.js
@@ -50,14 +50,61 @@ class SmarthomeSwitchAccessory {
     
     if(this.accessory.context.polling.timer && (!this.accessory.context.polling.exclude.includes(this.accessory.context.config.type) && !this.accessory.context.polling.exclude.includes(this.accessory.context.config.subtype) && !this.accessory.context.polling.exclude.includes(this.accessory.displayName))){
  
-      service.getCharacteristic(this.api.hap.Characteristic.On)
-        .on('set', this.handler.set.bind(this, this.accessory, this.api.hap.Service.Switch, this.api.hap.Characteristic.On, 'smarthome-switch', this.accessory.context.config.options));
+      if(!this.accessory.context.config.readOnly){
+  
+        service.getCharacteristic(this.api.hap.Characteristic.On)
+          .on('set', this.handler.set.bind(this, this.accessory, this.api.hap.Service.Switch, this.api.hap.Characteristic.On, 'smarthome-switch', this.accessory.context.config.options));
+  
+      } else {
+  
+        service.getCharacteristic(this.api.hap.Characteristic.On)
+          .on('set', (state, callback) => {
+          
+            Logger.info('Can not be switched ' + (state ? 'ON' : 'OFF') + ' - "readOnly" is active!', this.accessory.displayName);
+          
+            setTimeout(() => {
+            
+              service
+                .getCharacteristic(this.api.hap.Characteristic.On)
+                .updateValue(!state);
+            
+            }, 1000);
+            
+            callback(null);
+          
+          });
+  
+      }
  
     } else {
  
-      service.getCharacteristic(this.api.hap.Characteristic.On)
-        .on('get', this.handler.get.bind(this, this.accessory, this.api.hap.Service.Switch, this.api.hap.Characteristic.On, 'smarthome-switch', this.accessory.context.config.options))
-        .on('set', this.handler.set.bind(this, this.accessory, this.api.hap.Service.Switch, this.api.hap.Characteristic.On, 'smarthome-switch', this.accessory.context.config.options));
+      if(!this.accessory.context.config.readOnly){
+   
+        service.getCharacteristic(this.api.hap.Characteristic.On)
+          .on('get', this.handler.get.bind(this, this.accessory, this.api.hap.Service.Switch, this.api.hap.Characteristic.On, 'smarthome-switch', this.accessory.context.config.options))
+          .on('set', this.handler.set.bind(this, this.accessory, this.api.hap.Service.Switch, this.api.hap.Characteristic.On, 'smarthome-switch', this.accessory.context.config.options));
+   
+      } else {
+   
+        service.getCharacteristic(this.api.hap.Characteristic.On)
+          .on('get', this.handler.get.bind(this, this.accessory, this.api.hap.Service.Switch, this.api.hap.Characteristic.On, 'smarthome-switch', this.accessory.context.config.options))
+          .on('set', (state, callback) => {
+          
+            Logger.info('Can not be switched ' + (state ? 'ON' : 'OFF') + ' - "readOnly" is active!', this.accessory.displayName);
+          
+            setTimeout(() => {
+            
+              service
+                .getCharacteristic(this.api.hap.Characteristic.On)
+                .updateValue(!state);
+            
+            }, 1000);
+            
+            callback(null);
+          
+          });
+   
+      }
  
     }
     
