1644afc20f04ef252ede2b6353b3aa4066029ebc
Seyd <34152761+SeydX@users.noreply.github.com>
2020-11-15 15:52:21 +0100

    Removed adaptiveLighting..
    Not supported atm, coming back with hb v1.3

--- src/accessories/smarthome/lightbulb.js
+++ src/accessories/smarthome/lightbulb.js
@@ -28,9 +28,14 @@ class SmarthomeLightbulbAccessory {
       service = this.accessory.addService(this.api.hap.Service.Lightbulb, this.accessory.displayName, this.accessory.context.config.subtype);
     }
     
-    if(this.accessory.context.config.brightness && !service.testCharacteristic(this.api.hap.Characteristic.Brightness))
-      service.addCharacteristic(this.api.hap.Characteristic.Brightness);  
-    
+    if(this.accessory.context.config.brightness){
+      if(!service.testCharacteristic(this.api.hap.Characteristic.Brightness))
+        service.addCharacteristic(this.api.hap.Characteristic.Brightness);  
+    } else {
+      if(service.testCharacteristic(this.api.hap.Characteristic.Brightness) && !this.accessory.context.config.color)
+        service.removeCharacteristic(service.getCharacteristic(this.api.hap.Characteristic.Brightness));  
+    }
+
     if(this.accessory.context.config.color){
     
       if(!service.testCharacteristic(this.api.hap.Characteristic.Brightness))
@@ -40,12 +45,12 @@ class SmarthomeLightbulbAccessory {
         service.addCharacteristic(this.api.hap.Characteristic.Hue); 
    
       if(!service.testCharacteristic(this.api.hap.Characteristic.Saturation))
-        service.addCharacteristic(this.api.hap.Characteristic.Saturation); 
+        service.addCharacteristic(this.api.hap.Characteristic.Saturation);
    
       if(!service.testCharacteristic(this.api.hap.Characteristic.ColorTemperature))
         service.addCharacteristic(this.api.hap.Characteristic.ColorTemperature);                  
       
-      if(this.api.versionGreaterOrEqual('v1.3.0-beta.23')){
+      /*if(this.api.versionGreaterOrEqual('v1.3.0-beta.23')){
     
         this.adaptiveLightingController  = new this.api.hap.AdaptiveLightingController(service, {
           controllerMode: this.api.hap.AdaptiveLightingControllerMode.AUTOMATIC,
@@ -54,7 +59,21 @@ class SmarthomeLightbulbAccessory {
         this.accessory.configureController(this.adaptiveLightingController);
         this.accessory.adaptiveLightingController = this.adaptiveLightingController;
       
-      }
+      }*/
+    
+    } else {
+    
+      if(service.testCharacteristic(this.api.hap.Characteristic.Brightness))
+        service.removeCharacteristic(service.getCharacteristic(this.api.hap.Characteristic.Brightness));   
+   
+      if(service.testCharacteristic(this.api.hap.Characteristic.Hue))
+        service.removeCharacteristic(service.getCharacteristic(this.api.hap.Characteristic.Hue));   
+   
+      if(service.testCharacteristic(this.api.hap.Characteristic.Saturation))
+        service.removeCharacteristic(service.getCharacteristic(this.api.hap.Characteristic.Saturation));  
+   
+      if(service.testCharacteristic(this.api.hap.Characteristic.ColorTemperature))
+        service.removeCharacteristic(service.getCharacteristic(this.api.hap.Characteristic.ColorTemperature));    
     
     }                                                                                             
     
@@ -87,7 +106,7 @@ class SmarthomeLightbulbAccessory {
       service.getCharacteristic(this.api.hap.Characteristic.Saturation)
         .on('set', this.handler.set.bind(this, this.accessory, this.api.hap.Service.Lightbulb, this.api.hap.Characteristic.Saturation, 'smarthome-lightbulb', 'color'));  
    
-    }          
+    }         
                    
   }
 
